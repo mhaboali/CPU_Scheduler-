c9df5b3d5c6db47ad018fd0e508099e25f3405fb
mohamedhessen <36520890+mohamedhessen@users.noreply.github.com>
2019-04-12 23:28:30 -0700

    Update RR.cs

--- CPU_Schedular_GUI/odo/RR.cs
+++ CPU_Schedular_GUI/odo/RR.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,9 +13,9 @@ namespace odo
     class RR : process
     {
 
-        public int number, waiting_time, avr_time;//processNo
+        public int number,waiting_time,avr_time;//processNo
         public int[] arrival;
-        public int[] waiting;
+         public int[]waiting;
         public List<int> Dur;
         public List<int> d = new List<int>();
         public List<int> plot = new List<int>();
@@ -23,7 +23,7 @@ namespace odo
 
         public List<int> process_no = new List<int>();
         public process[] pp;
-        public int j = 0, i = 0, q, z = 0, w = 0;
+        public int j = 0, i = 0, q, z=0,w=0;
 
         public RR(int num, int r, List<int> process_dur, int[] process_arriv)
         {
@@ -32,7 +32,7 @@ namespace odo
             Dur = process_dur;
             arrival = process_arriv;
         }
-
+       
 
         public void sets()
         {
@@ -46,18 +46,33 @@ namespace odo
                 i++;
             }
             Array.Sort(arrival);
-            int e = 0;
+            int e = 0,ee=0;
+            int[] ww;
+            ww = new int[100];
             for (int k = 0; (k < number) && (z != number); )
             {
+
+                if (e != 0) 
+                {  
+                for(int s=0;s<e;s++)
+                {
+                    if (k == ww[s])
+                    {  k++; }
+                }
+                }
                 if (pp[k].getArrival_time() == arrival[e])
                 {
                     Dur[e] = d[k];
                     process_no.Add(k);
+                    ww[e] = k;
+                    ee++;
                     k = 0;
                     e++;
                     z++;
                 }
-                else k++;
+
+                else {  k++; }
+           
             }
             int t = 0;
             waiting = new int[number];    // sorted processes
@@ -71,7 +86,7 @@ namespace odo
                     plot.Add(q);
                     time += q;
                     flag.Add("p" + process_no[j]);
-                    if (j < number)
+                    if (j < number-1)
                     {
                         if (arrival[j + 1] <= time)
                         {
@@ -93,24 +108,24 @@ namespace odo
                 else
                 {
                     plot.Add(Dur[j]);
-                    flag.Add("p" + process_no[j]);
-                    time += Dur[j];
+                    flag.Add( "p" + process_no[j]);
+                    time+=Dur[j];
                     w += Dur[j];
                     waiting[t] = w - pp[process_no[j]].getDuration() - pp[process_no[j]].getArrival_time();
                     x--;
                     t++;
                     j++;
                 }
-
+                
             }
-            for (int k = 0; k < number; k++)
+               for (int k = 0; k < number; k++)
                 waiting_time += waiting[k];
             avr_time = waiting_time / number;
         }
+        }
     }
-}
 
-
-
-
+        
+        
+      
 
